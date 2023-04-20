de517636ad0820a197a03c71b07a55e0040d3e48
Voj7us <119515230+Voj7us@users.noreply.github.com>
2023-04-20 12:27:09 +0200

    Add files via upload

---
+++ Zgadywanka.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zgadywanka {
+    public static void main(String[] args) {
+        int i = 0;
+        int los;
+        int odp;
+        Random random = new Random();
+        los = random.nextInt(10) + 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            i++;
+            System.out.println("Podaj liczbę od 1 do 10:");
+            odp = scanner.nextInt();
+            if (odp > los) {
+                System.out.println("Mniej");
+            } else if (odp < los) {
+                System.out.println("Więcej");
+            }
+        } while (los != odp);
+        System.out.println("Brawo odgadłeś za " + i + " razem.");
+        System.out.println("Koniec gry!");
+    }
+}
